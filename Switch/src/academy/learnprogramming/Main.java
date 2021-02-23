7e3ec280fa146bcc43604a2255235c3d4de19a80
Emerson Aagaard <emerson.aagaard@gmail.com>
2021-02-23 14:02:37 -0600

    switch statements

---
+++ Switch/src/academy/learnprogramming/Main.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int value = 1;
+
+        switch(value) {
+            case 1:
+                System.out.println("the value is 1");
+                break;
+            case 2:
+                System.out.println("the value is 2");
+                break;
+            default:
+                System.out.println("the value was something else");
+        }
+
+    }
+}
